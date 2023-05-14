ef7f31cc3a3e08abda10bb8f109d143af4ac2ce6
SkyyySi <rasti.b@protonmail.com>
2023-05-14 15:17:39 +0200

    various changes

--- config/syrics/search_song_id.py
+++ config/syrics/search_song_id.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 from syrics.api import Spotify
+from typing import Optional
 import sys
 import json
 
 
-def get_song_data(client: Spotify, title: str) -> dict:
-	return client.search(title, type="track", limit=1)["tracks"]["items"][0]
+# -> tuple[bool, dict]
+def get_song_data(client: Spotify, title: str):
+	tracks: list = client.search(title, type="track", limit=1)["tracks"]["items"]
+
+	if len(tracks) < 1:
+		return (False, None)
+
+	return (True, tracks[0])
 
 
 def main(argv: list[str]):
@@ -13,8 +20,8 @@ def main(argv: list[str]):
 		config = json.load(file)
 
 	client = Spotify(config["sp_dc"])
-	song = get_song_data(client, argv[1])
-	return song["id"]
+	found, song = get_song_data(client, argv[1])
+	return song["id"] if found else ""
 
 
 if __name__ == "__main__":
